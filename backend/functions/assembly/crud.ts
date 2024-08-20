cb604d951cd61b36d2131ba649c1a6e09bcb2021
Stephanie Dietz <49788645+StephDietz@users.noreply.github.com>
2024-08-19 20:35:10 -0500

    Initial commit

---
+++ backend/functions/assembly/crud.ts
@@ -0,0 +1,282 @@
+import { collections } from "@hypermode/functions-as";
+import { Product, Cart, CartItem, consts } from "./types";
+
+export function upsertProduct(
+  id: string,
+  name: string,
+  category: string,
+  price: f32,
+  description: string,
+  image: string,
+  stars: f32,
+  isStocked: boolean,
+): string {
+  let result = collections.upsert(consts.productNameCollection, id, name);
+  if (!result.isSuccessful) {
+    return result.error;
+  }
+  result = collections.upsert(consts.productCategoryCollection, id, category);
+  if (!result.isSuccessful) {
+    return result.error;
+  }
+  result = collections.upsert(
+    consts.productPriceCollection,
+    id,
+    price.toString(),
+  );
+  if (!result.isSuccessful) {
+    return result.error;
+  }
+  result = collections.upsert(
+    consts.productDescriptionCollection,
+    id,
+    description,
+  );
+  if (!result.isSuccessful) {
+    return result.error;
+  }
+  result = collections.upsert(consts.productImageCollection, id, image);
+  if (!result.isSuccessful) {
+    return result.error;
+  }
+  result = collections.upsert(
+    consts.productStarCollection,
+    id,
+    stars.toString(),
+  );
+  if (!result.isSuccessful) {
+    return result.error;
+  }
+  result = collections.upsert(
+    consts.isProductStockedCollection,
+    id,
+    isStocked.toString(),
+  );
+  if (!result.isSuccessful) {
+    return result.error;
+  }
+  return id;
+}
+
+export function upsertProducts(
+  ids: string[],
+  names: string[],
+  categories: string[],
+  prices: f32[],
+  descriptions: string[],
+  images: string[],
+  stars: f32[],
+  isStockedArray: boolean[],
+): string[] {
+  const errors: string[] = [];
+
+  if (
+    ids.length !== names.length ||
+    ids.length !== categories.length ||
+    ids.length !== prices.length ||
+    ids.length !== descriptions.length ||
+    ids.length !== images.length ||
+    ids.length !== stars.length ||
+    ids.length !== isStockedArray.length
+  ) {
+    errors.push("Length of all arrays must be the same");
+    return errors;
+  }
+  let result = collections.upsertBatch(
+    consts.productNameCollection,
+    ids,
+    names,
+  );
+
+  if (!result.isSuccessful) {
+    errors.push(result.error);
+    return errors;
+  }
+
+  result = collections.upsertBatch(
+    consts.productCategoryCollection,
+    ids,
+    categories,
+  );
+  if (!result.isSuccessful) {
+    errors.push(result.error);
+    return errors;
+  }
+  result = collections.upsertBatch(
+    consts.productPriceCollection,
+    ids,
+    prices.map<string>((x) => x.toString()),
+  );
+  if (!result.isSuccessful) {
+    errors.push(result.error);
+    return errors;
+  }
+
+  result = collections.upsertBatch(
+    consts.productDescriptionCollection,
+    ids,
+    descriptions,
+  );
+  if (!result.isSuccessful) {
+    errors.push(result.error);
+    return errors;
+  }
+
+  result = collections.upsertBatch(consts.productImageCollection, ids, images);
+  if (!result.isSuccessful) {
+    errors.push(result.error);
+    return errors;
+  }
+
+  result = collections.upsertBatch(
+    consts.productStarCollection,
+    ids,
+    stars.map<string>((x) => x.toString()),
+  );
+  if (!result.isSuccessful) {
+    errors.push(result.error);
+    return errors;
+  }
+
+  result = collections.upsertBatch(
+    consts.isProductStockedCollection,
+    ids,
+    isStockedArray.map<string>((x) => x.toString()),
+  );
+  if (!result.isSuccessful) {
+    errors.push(result.error);
+    return errors;
+  }
+
+  return ids;
+}
+
+export function deleteProduct(id: string): string {
+  let result = collections.remove(consts.productNameCollection, id);
+  if (!result.isSuccessful) {
+    return result.error;
+  }
+  result = collections.remove(consts.productCategoryCollection, id);
+  if (!result.isSuccessful) {
+    return result.error;
+  }
+  result = collections.remove(consts.productPriceCollection, id);
+  if (!result.isSuccessful) {
+    return result.error;
+  }
+  result = collections.remove(consts.productDescriptionCollection, id);
+  if (!result.isSuccessful) {
+    return result.error;
+  }
+  result = collections.remove(consts.productImageCollection, id);
+  if (!result.isSuccessful) {
+    return result.error;
+  }
+  result = collections.remove(consts.productStarCollection, id);
+  if (!result.isSuccessful) {
+    return result.error;
+  }
+  result = collections.remove(consts.isProductStockedCollection, id);
+  if (!result.isSuccessful) {
+    return result.error;
+  }
+  return "success";
+}
+
+export function deleteProducts(ids: string[]): string {
+  for (let i = 0; i < ids.length; i++) {
+    const res = deleteProduct(ids[i]);
+    if (res !== "success") {
+      return "Error deleting product with id: " + ids[i];
+    }
+  }
+
+  return "success";
+}
+
+export function getProduct(id: string): Product {
+  const name = collections.getText(consts.productNameCollection, id);
+  const category = collections.getText(consts.productCategoryCollection, id);
+  const priceRes = collections.getText(consts.productPriceCollection, id);
+  const price = parseFloat(priceRes);
+  const description = collections.getText(
+    consts.productDescriptionCollection,
+    id,
+  );
+  const image = collections.getText(consts.productImageCollection, id);
+  const starRes = collections.getText(consts.productStarCollection, id);
+  const stars = parseFloat(starRes);
+  const isStockedRes = collections.getText(
+    consts.isProductStockedCollection,
+    id,
+  );
+  const isStocked = isStockedRes === "true";
+
+  return new Product(
+    id,
+    name,
+    category,
+    f32(price),
+    description,
+    image,
+    f32(stars),
+    isStocked,
+  );
+}
+
+export function getProducts(ids: string[]): Product[] {
+  const products: Product[] = [];
+  for (let i = 0; i < ids.length; i++) {
+    products.push(getProduct(ids[i]));
+  }
+  return products;
+}
+
+// function serializeCartItem(item: CartItem): string {
+//   return `${item.productId}:${item.quantity.toString()}`;
+// }
+
+// function deserializeCartItem(itemStr: string): CartItem {
+//   const parts = itemStr.split(":");
+//   return new CartItem(parts[0], parseFloat(parts[1]));
+// }
+
+// function serializeCart(cart: Cart): string {
+//   let serializedItems = "";
+//   for (let i = 0; i < cart.items.length; i++) {
+//     if (i > 0) {
+//       serializedItems += ",";
+//     }
+//     serializedItems += serializeCartItem(cart.items[i]);
+//   }
+//   return serializedItems;
+// }
+
+// function deserializeCart(cartId: string, cartStr: string): Cart {
+//   const items: CartItem[] = [];
+//   const itemStrings = cartStr.split(",");
+//   for (let i = 0; i < itemStrings.length; i++) {
+//     items.push(deserializeCartItem(itemStrings[i]));
+//   }
+//   return new Cart(cartId, items);
+// }
+
+// export function upsertCart(cart: Cart): string {
+//   const cartStr = serializeCart(cart);
+//   const result = collections.upsert(
+//     consts.cartCollection,
+//     cart.cartId,
+//     cartStr,
+//   );
+//   return result.isSuccessful ? "success" : result.error;
+// }
+
+// export function getCart(cartId: string): Cart {
+//   const cartStr = collections.getText(consts.cartCollection, cartId);
+//   return cartStr ? deserializeCart(cartId, cartStr) : new Cart(cartId);
+// }
+
+// export function deleteCart(cartId: string): string {
+//   const result = collections.remove(consts.cartCollection, cartId);
+//   return result.isSuccessful ? "success" : result.error;
+// }
---
+++ backend/functions/assembly/search.ts
@@ -0,0 +1,113 @@
+import { models } from "@hypermode/functions-as";
+import { EmbeddingsModel } from "@hypermode/models-as/models/experimental/embeddings";
+import { collections } from "@hypermode/functions-as";
+import { getProduct } from "./crud";
+import { ProductSearchResult, ProductSearchObject, consts } from "./types";
+
+export function searchProducts(
+  query: string,
+  maxItems: i32,
+  thresholdStars: f32 = 0.0,
+): ProductSearchResult {
+  const productSearchRes = new ProductSearchResult(
+    consts.productNameCollection,
+    consts.searchMethod,
+    "success",
+    "",
+  );
+
+  const semanticSearchRes = collections.search(
+    consts.productNameCollection,
+    consts.searchMethod,
+    query,
+    maxItems,
+    true,
+  );
+
+  if (!semanticSearchRes.isSuccessful) {
+    productSearchRes.status = semanticSearchRes.status;
+    productSearchRes.error = semanticSearchRes.error;
+
+    return productSearchRes;
+  }
+
+  const rankedResults = reRankAndFilterSearchResultObjects(
+    semanticSearchRes.objects,
+    thresholdStars,
+  );
+
+  for (let i = 0; i < rankedResults.length; i++) {
+    const searchObj = getSearchObject(
+      rankedResults[i].key,
+      rankedResults[i].score,
+      rankedResults[i].distance,
+    );
+    productSearchRes.searchObjs.unshift(searchObj);
+  }
+
+  return productSearchRes;
+}
+
+function getSearchObject(
+  key: string,
+  score: f64,
+  distance: f64,
+): ProductSearchObject {
+  return new ProductSearchObject(getProduct(key), score, distance);
+}
+
+function reRankAndFilterSearchResultObjects(
+  objs: collections.CollectionSearchResultObject[],
+  thresholdStars: f32,
+): collections.CollectionSearchResultObject[] {
+  for (let i = 0; i < objs.length; i++) {
+    const starRes = collections.getText(
+      consts.productStarCollection,
+      objs[i].key,
+    );
+    const stars = parseFloat(starRes);
+
+    const inStockRes = collections.getText(
+      consts.isProductStockedCollection,
+      objs[i].key,
+    );
+    const inStock = inStockRes === "true";
+
+    if (!inStock) {
+      objs[i].score *= 0.5;
+    }
+    objs[i].score *= stars * 0.1;
+  }
+
+  objs.sort((a, b) => {
+    if (a.score < b.score) {
+      return -1;
+    } else if (a.score > b.score) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  const filteredResults: collections.CollectionSearchResultObject[] = [];
+  for (let i = 0; i < objs.length; i++) {
+    const starRes = collections.getText(
+      consts.productStarCollection,
+      objs[i].key,
+    );
+    const stars = parseFloat(starRes);
+    if (stars >= thresholdStars) {
+      filteredResults.push(objs[i]);
+    }
+  }
+
+  return filteredResults;
+}
+
+export function miniLMEmbed(texts: string[]): f32[][] {
+  const model = models.getModel<EmbeddingsModel>(consts.embeddingModel);
+  const input = model.createInput(texts);
+  const output = model.invoke(input);
+
+  return output.predictions;
+}
